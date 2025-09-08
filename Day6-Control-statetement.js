cacebdc79254be96204ae32187ec2506576d20f0
Mayur111-code <mayurborse7440@gmail.com>
2025-09-09 00:15:17 +0530

    todays code

---
+++ Day6-Control-statetement.js
@@ -0,0 +1,63 @@
+//control sattement
+// types 
+//consditional statement 
+//it check the conditions 
+
+// if 
+// if else 
+//     if else if else 
+//         switch
+
+
+// looping statement
+//iterating
+
+//if := it checks the only single condition
+
+//synatx
+// if(condtion/true/non-zero /value){
+//     //code excute
+// }
+
+let userName = "Mayur";
+if (true
+) {
+    //alert(userName);
+}
+
+//checks only one condition
+let age = 19;
+if (age>=18) {
+    //alert("You are Eligibale")
+} if(age<18){
+    //alert("Sorry not Eligibale")
+}
+
+//if else -used to 2 conditions
+
+// if (condition) {
+
+// } else {
+
+// }
+
+let age1 = 5;
+if (age1>=18) {
+    //alert("congrats");
+} else{
+   // alert("Sorry")
+}
+//dailog box
+//Used to take data dynamically from the user
+
+
+//prompt()
+
+let giveage = prompt('Enter your age' , 18);
+//alert(giveage);
+if (giveage>=18) {
+    alert(`congrats..!! your age is ${giveage}`)
+} else {
+    alert(`sorry your age is ${giveage}`)
+}
+
