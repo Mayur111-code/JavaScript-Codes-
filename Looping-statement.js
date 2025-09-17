18154caeae530ad5987e25e4ff9a3c073da1af8f
Mayur111-code <mayurborse7440@gmail.com>
2025-09-17 13:08:39 +0530

    js update

--- Looping-statement.js
+++ Looping-statement.js
@@ -6,29 +6,29 @@
 // while 
 
 // do while
-//for loop
-//for in, for of, for each, map
+// for loop
+// for in, for of, for each, map
 
 
-// document.write("<h1>Hello Mayur..!!</h1>")
-// //while
+document.write("<h1>Hello Mayur..!!</h1>")
+//while
 
-// // let i = 0;
-// // while (condition) {
-// //     //block of codee 
-// // }
+let i = 0;
+while (condition) {
+    //block of codee 
+}
 
-// let i = 1;
-// while(i<=10){
-//     document.write("<h1>Hello Mayur..!! "+i+" </h1>")
-//     i++;
-// }
+let i = 1;
+while(i<=10){
+    document.write("<h1>Hello Mayur..!! "+i+" </h1>")
+    i++;
+}
 
-// let no = 1;
-// while(no<=50){
-//     document.write("<h2>javascript</h2>");
-//     no++;
-// }
+let no = 1;
+while(no<=50){
+    document.write("<h2>javascript</h2>");
+    no++;
+}
 
 let num = 0;
 while(num<=100){
@@ -38,14 +38,70 @@ document.write(num +" " )
 
 document.write("<h1>*********************************************</h1>")
 
-// let num1 = 0;
-// while(num1<=100){
-// document.write(num1 +" " )
-//     num1+=2;
-// }
+let num1 = 0;
+while(num1<=100){
+document.write(num1 +" " )
+    num1+=2;
+}
+
+document.write("<h1**********************************</h1>");
 
 let num1 = 0;
 while(num1<=100){
 document.write(num1 +" " )
     num1++;
+}
+let i=0; 
+var sum = 0;
+while (i<=100){
+    if (i % 2 == 0){
+        document.write(i + "");
+        sum +=i
+    }
+    i++;
+}
+
+console.log("tooal suum is  : " +sum);
+
+
+
+ let m = 100;
+ let add = 0
+ while(m >= 1) {
+   console.log(m);
+    add-=m;
+      
+m--
+    
+ }
+ console.log(add );
+ 
+ //nested while loop
+
+ let mm = 2;
+  while(mm <= 15) {
+    console.log(mm)
+
+
+    let kk = 1 ;
+ while (kk <= 10) {
+    console.log(kk);
+    kk++
+    
+ }
+ mm++;
+    
+  }
+
+let xy = 2;
+
+while(xy<=15){
+    document.write("<h1>"+xy+"</h1>")
+     
+    let ab = 1;
+    while(ab <= 10){
+        document.write(xy*ab + "")
+        ab++;
+    }
+    xy++;
 }
